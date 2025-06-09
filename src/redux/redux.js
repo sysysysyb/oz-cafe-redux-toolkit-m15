bb6843d6ccc32c82f56285ae42c388052affa769
SEOYOUNG BAEK <baeksy6067@gmail.com>
2025-06-09 14:16:50 +0900

    ♻️ Refactor: createSlice, configureStore 이용해서 기존 상태를 redux-toolkit으로 리팩토링

---
+++ src/redux/redux.js
@@ -0,0 +1,42 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import data from "../assets/data";
+
+const menuSlice = createSlice({
+  name: "menu",
+  initialState: data.menu,
+  reducers: {},
+});
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: [],
+  reducers: {
+    addToCart(state, action) {
+      return [...state, action.payload];
+    },
+    removeFromCart(state, action) {
+      return state.filter((_, idx) => action.payload.id !== idx);
+    },
+  },
+});
+
+export const store = configureStore({
+  reducer: {
+    menu: menuSlice.reducer,
+    cart: cartSlice.reducer,
+  },
+});
+
+export const addToCart = (options, quantity, id) => {
+  return {
+    type: "addToCart",
+    payload: { options, quantity, id },
+  };
+};
+
+export const removeFromCart = (id) => {
+  return {
+    type: "removeFromCart",
+    payload: { id },
+  };
+};
